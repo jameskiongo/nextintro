d9588b93148245e2b525c2e5cc8d5225303b97f9
jameskiongo <kiongojames2000@gmail.com>
2023-04-09 14:10:46 +0300

    next intro

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By James{' '}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
